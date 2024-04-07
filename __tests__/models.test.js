f06628dafced0c7ce29f7bb4da175b59be4d4c14
Mak Trnka <maktrnka@comcast.net>
2024-04-07 16:26:05 -0700

    Update models.test.js

--- __tests__/models.test.js
+++ __tests__/models.test.js
@@ -1,48 +1,25 @@
-// models.test.js
-
-const { sequelize, Player, Team } = require('../src/models');
+'use strict';
+const { sequelize, Country } = require('../../src/models');
 
 // setup script
 beforeAll(async () => {
   await sequelize.sync();
 });
 
-// PLAYER MODEL
-describe('Player Model', () => {
-  it('Should create a player row', async () => {
-    const jokic = await Player.create({
-      name: 'Jokic',
-      position: 'Center',
-      points: 32, // Adjusted to lowercase points
-      rebounds: 9, // Adjusted to lowercase rebounds
-      assists: 12 // Adjusted to lowercase assists
+describe('Country Model', () => {
+  it('Should create a country row', async () => {
+    const testCountry = await Country.create({
+      name: 'Testland',
+      population: 1000000,
+      HDI: 0.9,
+      nativeLanguage: 'English',
     });
 
-    expect(jokic.name).toEqual('Jokic');
-    expect(jokic.position).toEqual('Center'); // Adjusted to position
-    expect(jokic.points).toEqual(32);
-    expect(jokic.rebounds).toEqual(9);
-    expect(jokic.assists).toEqual(12);
+    expect(testCountry.name).toEqual('Testland');
+    expect(testCountry.population).toEqual(1000000);
+    expect(testCountry.HDI).toEqual(0.9);
+    expect(testCountry.nativeLanguage).toEqual('English');
   });
 
-  xit('Should read a player from the table', async () => {});
-});
-
-// TEAM MODEL
-describe('Team Model', () => {
-  it('Should create a team row', async () => {
-    const nuggets = await Team.create({
-      name: 'Nuggets',
-      location: 'Denver',
-      wins: 32,
-      losses: 10
-    });
-
-    expect(Team.name).toEqual('Nuggets'); // Adjusted to nuggets
-    expect(Team.location).toEqual('Denver');
-    expect(Team.wins).toEqual(32);
-    expect(Team.losses).toEqual(10);
-  });
-
-  xit('Should read a team from the table', async () => {});
+  it('Should read a country from the table', async () => {});
 });
